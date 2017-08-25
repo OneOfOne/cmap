e1ec38a7f016c9fdf724613d005502a688a98a7d
OneOfOne <oneofone@gmail.com>
2017-08-25 19:58:10 +0200

    regen

--- utils.go
+++ utils.go
@@ -1,7 +1,7 @@
 //go:generate go install github.com/OneOfOne/genx/...
-//go:generate genx -pkg github.com/OneOfOne/cmap -v -m -t KT=interface{},VT=interface{} -o ./cmap_iface_iface.go
-//go:generate genx -pkg github.com/OneOfOne/cmap -v -m -n stringcmap -t KT=string,VT=interface{} -o ./stringcmap/cmap_string_iface.go
-//go:generate genx -pkg github.com/OneOfOne/cmap -v -m -n uint64cmap -t KT=uint64,VT=interface{} -o ./uint64cmap/cmap_uint64_iface.go
+//go:generate genx -pkg github.com/OneOfOne/cmap -v -t KT=interface{},VT=interface{} -o ./cmap_iface_iface.go
+//go:generate genx -pkg github.com/OneOfOne/cmap -v -n stringcmap -t KT=string,VT=interface{} -o ./stringcmap/cmap_string_iface.go
+//go:generate genx -pkg github.com/OneOfOne/cmap -v -n u64cmap -t KT=uint64,VT=interface{} -o ./u64cmap/cmap_u64_iface.go
 //go:generate gometalinter --aggregate --cyclo-over=17 ./...
 
 package cmap
